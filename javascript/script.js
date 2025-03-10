666a10633eaee6f2c0776f88ef09959160ac6c25
jhumphrey11 <testme989906@gmail.com>
2025-03-09 23:18:11 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,14 @@ imageElement.addEventListener("mouseover", () => {
 imageElement.addEventListener("mouseout", () => {
     imageElement.src = "image1.jpg"; // Image changes back when mouse leaves
 });
+
+/* the video container */
+.video-container {
+    width: 80%; 
+    margin: 0 auto; /* Center the video on the page */
+    text-align: center;
+}
+
+video {
+    border-radius: 10px; 
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
